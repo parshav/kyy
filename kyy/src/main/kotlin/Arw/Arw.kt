ca94b6be5055a531acacbc7bd90fbb0065f434d2
Parshav <parshav@live.com>
2018-07-04 12:37:55 -0500

    Minor work on Either

--- kyy/src/main/kotlin/Arw/Arw.kt
+++ kyy/src/main/kotlin/Arw/Arw.kt
@@ -1,12 +1,12 @@
 package Arw
 
 import Behavior.CoreLife
-import arrow.core.Either
+import arrow.core.*
 
 sealed class PlantState {
-    object Gone
-    class Alive(color: String)
-    class Seed(type: String)
+    object Gone : PlantState()
+    class Alive(color: String) : PlantState()
+    class Seed(type: String) : PlantState()
 }
 
 class Arw {
@@ -17,7 +17,10 @@ class Arw {
 object Arww : CoreLife {
 
     override fun onStart() {
-        val a = Either<Arw.PlantState.Gone, Arw.PlantState.Alive>
+
+        val a: Either<PlantState.Gone, PlantState.Alive> = PlantState.Gone.left()
+        val b: Option<PlantState> = PlantState.Alive("").some()
+        val c = Option.functor()
     }
 
     override fun onEnd() = Unit
